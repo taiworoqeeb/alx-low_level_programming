171892c2eba6c2e99bba3386155ed59dbcce0cf3
Raqeeb Taiwo <44765711+taiworoqeeb@users.noreply.github.com>
2022-03-18 10:29:57 +0100

    Add files via upload

---
+++ 0x02-functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - this function checks if 
+ * the input is a digit or not
+ * @c: c is the input tested
+ *
+ * Return: 1 if c is a digit
+ * 0: otherwise
+ */
+int _isdigit( int c)
+{
+	return(c >= '0' && c <= '9');
+}
